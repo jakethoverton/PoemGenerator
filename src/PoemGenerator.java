c7a21c6ff8ef4f0984950ea98cfd7f301c73e6c3
Jake <jakethoverton@gmail.com>
2019-01-31 14:31:03 -0500

    Added text file scanner and argument parsing

--- src/PoemGenerator.java
+++ src/PoemGenerator.java
@@ -2,7 +2,8 @@
 * @author Jacob Thomas Overton <jakethoverton@gmail.com>
 * This program generates a psuedorandom poem according to rules provided by Sift.
 */
-import java.util.*
+import java.util.*;
+import java.io.*;
 
 /**
 * This class holds the main method for generating the poem.
@@ -13,6 +14,19 @@ public class PoemGenerator{
 	* @param args arguments provided through the command line.
 	*/
 	public static void main (String args[]){
-		
+		if(args.length != 1){
+			System.out.println("Please supply a text file to parse as an argument.");
+			System.exit(1);
+		}
+		try {
+			File rules = new File(args[0]);	
+			Scanner scan = new Scanner(rules);
+			while(scan.hasNextLine()){
+				System.out.println(scan.nextLine());
+			}		
+		} catch(FileNotFoundException e){
+			System.out.println("File not found.");
+			System.exit(2);
+		}
 	}
 }
