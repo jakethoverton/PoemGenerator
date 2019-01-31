b31e3c221462051cdc0c104d42d0833d8ca514ae
Jake <jakethoverton@gmail.com>
2019-01-31 13:58:33 -0500

    Initial commit including test file and folder structure

---
+++ src/PoemGenerator.java
@@ -0,0 +1,18 @@
+/**
+* @author Jacob Thomas Overton <jakethoverton@gmail.com>
+* This program generates a psuedorandom poem according to rules provided by Sift.
+*/
+import java.util.*
+
+/**
+* This class holds the main method for generating the poem.
+*/
+public class PoemGenerator{
+	/**
+	* This is the main method.
+	* @param args arguments provided through the command line.
+	*/
+	public static void main (String args[]){
+		
+	}
+}
