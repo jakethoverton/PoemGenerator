3447302a5d8e3b1ebb0e2bdbcef1c8739b3299c4
Jake <jakethoverton@gmail.com>
2019-01-31 17:13:40 -0500

    Added randomness and recursive navigation through the array. Todo: handle <> elements

--- src/PoemGenerator.java
+++ src/PoemGenerator.java
@@ -26,7 +26,7 @@ public class PoemGenerator{
 			//Creates a scanner if file does not throw exception
 			Scanner scan = new Scanner(rules);
 			//Data Structure to store each token in text file
-			String parse[][] = new String[7][7];
+			String parse[][] = new String[8][7];
 			//Keeps track of what line of text file we are on
 			int lineCount = 0;
 			//Keeps track of which token on the line we are on
@@ -42,12 +42,48 @@ public class PoemGenerator{
 				lineCount++;
 				tokenCount = 0;
 			}
-			//Used for testing: Prints contents of parse array
-			System.out.println(Arrays.deepToString(parse));
+			drillDown(parse, 0, 0);
+			scan.close();
 		//Exits the program if File cannot be found
 		} catch(FileNotFoundException e){
 			System.out.println("File not found.");
 			System.exit(2);
 		}
+	}
+	/**
+	* Operates on each token in the array 
+	* @param parse 2D array containing data from rules file
+	* @param row integer containing what row of the array to access
+	* @param col integer containing what column of the array to access
+	*/
+	public static void drillDown(String parse[][], int row, int col){
+		if(parse[row][0] == null){
+			System.exit(0);
+		}else if(parse[row][col] == null){
+			drillDown(parse, row + 1, 0);
+		}
+		if(parse[row][col].contains("|")){
+			System.out.println(handleRandom(parse[row][col]) + " ");
+			drillDown(parse, row, col + 1);
+		} else {
+			System.out.println(parse[row][col] + " ");
+			drillDown(parse, row, col + 1);		
+		}
+	}
+	/**
+	* Chooses a random value from the pipe delimited string
+	* @param token The pipe delimited String
+	*/
+	public static String handleRandom(String token){
+		ArrayList<String> options = new ArrayList<String>();
+		Scanner scn = new Scanner(token);
+		scn.useDelimiter("\\|");
+		while(scn.hasNext()){
+			options.add(scn.next());
+		}
+		Random rand = new Random();
+		String selection = options.get(rand.nextInt(options.size()));
+		scn.close();
+		return selection;		
 	}
 }
